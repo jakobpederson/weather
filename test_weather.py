24bc84b7104961fa4f6cd60b62810a31bb6d7295
jakobpederson <jakob.pederson+github@gmail.com>
2017-05-29 15:09:34 -0500

    update

--- test_weather.py
+++ test_weather.py
@@ -3,9 +3,9 @@ import os
 import unittest
 import weather
 
-MISSING = -9999
+MISSING = '-9999'
 
-TEST_DATA_01 = [['a', '1981', 2, 3, MISSING], ['a', '1982', 3, 4, MISSING], ['a', '1983', 4, 5, 6], ['a', '1984', 6, 7, 8]]
+TEST_DATA_01 = [['1985', '2', '3', MISSING], ['1986', '3', '4', MISSING], ['1987', '4', '5', '6'], ['1988', '6', '7', '8']]
 
 TEST_DATA_02 = [['b', 1, 2, 3, 4], ['b', 2, 3, MISSING, MISSING], ['b', 3, 4, 5, 6], ['b', 5, 6, 7, 8]]
 
@@ -22,35 +22,43 @@ class WeatherTest(unittest.TestCase):
         self.assertEqual(0, self.weather.count_missing_precipitation(TEST_DATA_02))
 
     def test_write_missing_precip(self):
-        count = self.weather.count_missing_precipitation(TEST_DATA_01)
-        count_2 = self.weather.count_missing_precipitation(TEST_DATA_02)
-        list_of_days = self.weather.convert_to_days(TEST_DATA_01)
-        list_of_days_2 = self.weather.convert_to_days(TEST_DATA_02)
+        list_of_days = self.weather.convert_to_days('a', TEST_DATA_01)
+        # list_of_days_2 = self.weather.convert_to_days(TEST_DATA_02)
+        count = self.weather.count_missing_precipitation(list_of_days)
+        # count_2 = self.weather.count_missing_precipitation(TEST_DATA_02)
         self.weather.write_missing_precip(list_of_days, count)
-        self.weather.write_missing_precip(list_of_days_2, count_2)
+        # self.weather.write_missing_precip(list_of_days_2, count_2, app=True)
         expected = [['a', '2'], ['b', '0']]
         result = self.read_into_list_of_lists('MissingPrcpData.out')
         self.assertCountEqual(expected, result)
 
-    def test_get_max_min_by_year(self):
+    def test_get_avg_max_avg_min_by_year(self):
         list_of_days = self.weather.convert_to_days(TEST_DATA_01)
-        result = self.weather.get_max_min_by_year(list_of_days)
-        self.assertTrue(result['high']['1981'] == 2)
-        self.assertTrue(result['low']['1981'] == 3)
-        self.assertTrue(result['high']['1982'] == 3)
-        self.assertTrue(result['low']['1982'] == 4)
-        self.assertTrue(result['high']['1983'] == 4)
-        self.assertTrue(result['low']['1983'] == 5)
-        self.assertTrue(result['high']['1984'] == 6)
-        self.assertTrue(result['low']['1984'] == 7)
-        list_of_days = self.weather.convert_to_days(TEST_DATA_03)
-        result = self.weather.get_max_min_by_year(list_of_days)
-        self.assertTrue(result['high']['1981'] == 130)
-        self.assertTrue(result['low']['1981'] == 30)
+        result_high, result_low = self.weather.get_max_min_by_year(list_of_days)
+        self.assertEqual(2.0, result_high['1985'])
+        self.assertEqual(3.0, result_high['1986'])
+        self.assertEqual(4.0, result_high['1987'])
+        self.assertEqual(6.0, result_high['1988'])
+        self.assertEqual(3.0, result_low['1985'])
+        self.assertEqual(4.0, result_low['1986'])
+        self.assertEqual(5.0, result_low['1987'])
+        self.assertEqual(7.0, result_low['1988'])
+
+    def test_get_total_precip(self):
+        list_of_days = self.weather.convert_to_days(TEST_DATA_01)
+        self.assertEqual(6, self.weather.get_total_precip(list_of_days)['1987'])
+        self.assertEqual(8, self.weather.get_total_precip(list_of_days)['1988'])
+
+    def test_write_to_answer_2(self):
+        list_of_days = self.weather.convert_to_days('a', TEST_DATA_01)
+        high, low = self.weather.get_max_min_by_year(list_of_days)
+        precip = self.weather.get_total_precip(list_of_days)
+        result = self.weather.write_answer_2(list_of_days[0].file_name, high, low, precip)
+        self.assertCountEqual(self.read_into_list_of_lists('YearlyAverages.out'), result)
 
     def read_into_list_of_lists(self, file_name):
         results = []
-        with open(os.path.join(weather.DESKTOP, file_name)) as inputfile:
+        with open(os.path.join(weather.DESKTOP + 'answers/', file_name)) as inputfile:
             for line in inputfile:
                 results.append(line.strip().split('\t'))
         return results
--- weather.py
+++ weather.py
@@ -1,7 +1,8 @@
 from collections import namedtuple, defaultdict
 import os
+from argparse import ArgumentParser
 
-DESKTOP = os.path.join(os.path.expanduser("~"), "Desktop/answers/")
+DESKTOP = os.path.join(os.path.expanduser("~"), "Desktop/")
 
 Day = namedtuple('Day', ['file_name', 'date', 'high', 'low', 'precip'])
 MISSING = -9999
@@ -9,35 +10,100 @@ MISSING = -9999
 
 class Weather():
 
-    def convert_to_days(self, list_of_lists):
+    def convert_to_days(self, filename, list_of_lists):
         result = []
         for value in list_of_lists:
-            result.append(Day(file_name=value[0], date=value[1], high=value[2], low=value[3], precip=value[4]))
+            result.append(Day(file_name=filename, date=(value[0].strip()), high=float((value[1].strip())), low=float((value[2].strip())), precip=(value[3].strip())))
         return result
 
-    def count_missing_precipitation(self, list_of_lists):
+    def count_missing_precipitation(self, list_of_days):
         count = 0
-        list_of_days = self.convert_to_days(list_of_lists)
         for day in list_of_days:
             if day.high > MISSING and day.low > MISSING and day.precip == MISSING:
                 count += 1
         return count
 
-    def write_missing_precip(self, list_of_days, count):
-        try:
-            with open(os.path.join(DESKTOP, 'MissingPrcpData.out'), 'a') as f:
-                f.write(list_of_days[0].file_name + "\t" + str(count) + '\n')
-        except:
-            with open(os.path.join(DESKTOP, 'MissingPrcpData.out'), 'w') as f:
-                f.write(list_of_days[0].file_name + "\t" + str(count) + '\n')
+    def write_missing_precip(self, list_of_days, count, app=False):
+        answer = DESKTOP + 'answers/'
+
+
+        # if app:
+        #     with open(os.path.join(answer, 'MissingPrcpData.out'), 'a') as f:
+        #         f.write(list_of_days[0].file_name + "\t" + str(count) + '\n')
+
+        # else:
+        with open(os.path.join(answer, 'MissingPrcpData.out'), 'a') as f:
+            f.write(list_of_days[0].file_name + "\t" + str(count) + '\n')
 
     def get_max_min_by_year(self, list_of_days):
-        years_high = defaultdict(float)
-        years_low = defaultdict(float)
+        years_high = defaultdict(list)
+        years_low = defaultdict(list)
         for day in list_of_days:
-            years_high[day.date[:4]] = max(day.high, years_high[day.date[:4]])
-            if years_low[day.date[:4]]:
-                years_low[day.date[:4]] = min(day.low, years_low[day.date[:4]])
+            if day.high > MISSING:
+                years_high[day.date[:4]].append(float(day.high))
+            if day.low > MISSING:
+                years_low[day.date[:4]].append(float(day.low))
+        return self.get_averages(years_high, years_low)
+
+    def get_averages(self, years_high, years_low):
+        high_avg = defaultdict(lambda: float(-9999))
+        low_avg = defaultdict(lambda: float(-9999))
+        for i in range(1985, 2015):
+            key = str(i)
+            if len(years_high[key]) > 0:
+                high_avg[key] = sum(years_high[key])/len(years_high[key])
+            if len(years_low[key]) > 0:
+                low_avg[key] = sum(years_low[key])/len(years_low[key])
+        return high_avg, low_avg
+
+    def get_total_precip(self, list_of_days):
+        total_precip = defaultdict(list)
+        final_precip = defaultdict(int)
+        for day in list_of_days:
+            total_precip[day.date[:4]].append(float(day.precip))
+        for i in range(1985, 2015):
+            key = str(i)
+            if total_precip[key]:
+                final_precip[key] = sum(total_precip[key])/len(total_precip[key])
             else:
-                years_low[day.date[:4]] = day.low
-        return {'high': years_high, 'low': years_low}
+                final_precip[key] = MISSING
+        return final_precip
+
+    def write_answer_2(self, file_name, high_avg, low_avg, total_precip, app=False):
+        result = []
+
+        # if app:
+        #     with open(os.path.join(DESKTOP, 'YearlyAverages.out'), 'a') as f:
+        #         for i in range(1985, 2015):
+        #             key = str(i)
+        #             string_data = '{}\t{}\t{:0.2f}\t{:0.2f}\t{}'.format(file_name, key, high_avg[key], low_avg[key], str(total_precip[key]))
+        #             f.write(string_data + '\n')
+        #             result.append(string_data.split('\t'))
+
+        # else:
+        with open(os.path.join(DESKTOP, 'YearlyAverages.out'), 'a') as f:
+            for i in range(1985, 2015):
+                key = str(i)
+                string_data = '{}\t{}\t{:0.2f}\t{:0.2f}\t{}'.format(file_name, key, high_avg[key], low_avg[key], str(total_precip[key]))
+                f.write(string_data + '\n')
+                result.append(string_data.split('\t'))
+        return result
+
+
+if __name__ == "__main__":
+    path_wx_data = DESKTOP + 'wx_data/'
+    path_yld_data = DESKTOP + 'yld_data/'
+    g = Weather()
+    for filename in os.listdir(path_wx_data):
+        data = []
+        new_data = []
+        with open(os.path.join(path_wx_data, filename)) as inputfile:
+            for line in inputfile:
+                data.append(line.strip().split('\t'))
+            list_of_days = g.convert_to_days(filename, data)
+            count = g.count_missing_precipitation(list_of_days)
+            g.write_missing_precip(list_of_days, count, app=True)
+            high, low = g.get_max_min_by_year(list_of_days)
+            precip = g.get_total_precip(list_of_days)
+            g.write_answer_2(filename, high, low, precip)
+
