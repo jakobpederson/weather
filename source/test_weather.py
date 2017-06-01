3f1fca82d8daf8e92b912dddae78d2b3aa732d93
jakobpederson <jakob.pederson+github@gmail.com>
2017-05-31 20:37:07 -0500

    wip

--- source/test_weather.py
+++ source/test_weather.py
@@ -1,12 +1,22 @@
 from collections import namedtuple
 import os
+import sys
 import unittest
 import weather
 
-DESKTOP = os.path.join(os.path.expanduser('~'), 'Desktop' + '/')
-ANSWER = DESKTOP + 'answers' + '/'
+CODE_EXAM = os.path.abspath(os.path.join(__file__, "../.."))
+SOURCE = os.path.dirname(os.path.realpath(__file__))
+TEST_DATA = SOURCE + '/' + 'tests'
+ANSWER = CODE_EXAM + '/' + 'answers'
 MISSING = -9999
-PATH_WX = DESKTOP + 'wx_data' + '/'
+PATH_WX = CODE_EXAM + 'wx_data' + '/'
+FILE_LIST = [
+    'UC001.txt',
+    'UC002.txt',
+    'UC003.txt',
+    'UC004.txt',
+    'UC005.txt',
+    ]
 
 Day = namedtuple('Day', ['file_name', 'date', 'year', 'high', 'low', 'precip'])
 
@@ -19,21 +29,15 @@ class WeatherTest(unittest.TestCase):
             os.remove(ANSWER, 'MissingPrcpData.out')
             os.remove(ANSWER, 'YearlyAverages.out')
         except:
-            with open(ANSWER + 'MissingPrcpData.out', 'w') as f:
-                pass
-            with open(ANSWER + 'YearlyAverages.out', 'w') as f:
-                pass
+            open(ANSWER + 'MissingPrcpData.out', 'w')
+            open(ANSWER + 'YearlyAverages.out', 'w')
         self.c = weather.Weather()
-        result = []
-        with open(os.path.join(DESKTOP + 'wx_data' + '/', 'USC00110072.txt')):
-            result.extend(self.c.process_file('USC00110072.txt'))
-        self.master_list = result
 
     def test_read_line(self):
-        expected = [Day(file_name='USC00110072.txt', date=1985, high=-22.0, low=-128.0, precip=94.0)]
-        with open(os.path.join(DESKTOP + 'wx_data' + '/', 'USC00110072.txt')) as f:
+        expected = [Day(file_name='USC001.txt', date=19890101, year=1989, high=56.0, low=-50.0, precip=0.0)]
+        with open(os.path.join(TEST_DATA + '/', 'USC001.txt')) as f:
             line = f.readline()
-            result = self.c.convert_to_days('USC00110072.txt', line)
+            result = self.c.convert_to_days('USC001.txt', line)
             self.assertEqual(expected[0].file_name, result[0].file_name)
             self.assertEqual(expected[0].date, result[0].date)
             self.assertEqual(expected[0].high, result[0].high)
@@ -41,21 +45,45 @@ class WeatherTest(unittest.TestCase):
             self.assertEqual(expected[0].precip, result[0].precip)
 
     def test_process_file(self):
-        result = []
-        with open(os.path.join(DESKTOP + 'wx_data' + '/', 'USC00110072.txt')):
-            result.extend(self.c.process_file('USC00110072.txt'))
-        self.assertEqual(10865, len(result))
+        results = []
+        expected = [
+            Day(file_name='USC001.txt', date=19890101, year=1989, high=56.0, low=-50.0, precip=0.0),
+            Day(file_name='USC001.txt', date=19890102, year=1989, high=11.0, low=-56.0, precip=0.0),
+            Day(file_name='USC001.txt', date=19890103, year=1989, high=-33.0, low=-111.0, precip=0.0),
+            Day(file_name='USC001.txt', date=19890104, year=1989, high=-17.0, low=-133.0, precip=30.0),
+            Day(file_name='USC001.txt', date=19890105, year=1989, high=17.0, low=-106.0, precip=0.0)
+         ]
+        with open(os.path.join(TEST_DATA + '/', 'USC001.txt')):
+            results.extend(self.c.process_file(TEST_DATA, 'USC001.txt'))
+        self.assertEqual(5, len(results))
+        for result in results:
+            self.assertTrue(result in expected)
 
-    def test_get_missing_precip(self):
-        precips = self.c.get_missing_precip_dates(self.master_list)
-        self.c.write_precip_dates(precips)
+    def test_get_missing_prcp_data(self):
+        data = []
+        with open(os.path.join(TEST_DATA + '/', 'USC002.txt')):
+            data.extend(self.c.process_file(TEST_DATA, 'USC002.txt'))
+        results = self.c.get_missing_prcp_data(data)
+        self.assertTrue('USC002.txt' in results.keys())
+        self.assertTrue(1 in results.values())
         self.assertTrue('MissingPrcpData.out' in os.listdir(ANSWER))
 
-    def test_get_averages(self):
-        expected, _ = self.c.get_averages_data(self.master_list)
-        self.c.write_averages_dates(expected)
+    def test_get_yearly_averages(self):
+        data = []
+        with open(os.path.join(TEST_DATA + '/', 'USC001.txt')):
+            data.extend(self.c.process_file(TEST_DATA, 'USC001.txt'))
+        results = self.c.get_yearly_averages(data)
+        self.assertTrue(results[1989] == ('USC001.txt', 1989, 0.68000000000000005, -9.120000000000001, 3.0))
         self.assertTrue('YearlyAverages.out' in os.listdir(ANSWER))
 
     def test_get_year_histogram(self):
-        _, file_list = self.c.get_averages_data(self.master_list)
-        self.fail(self.c.get_year_histogram(file_list))
+        data = []
+        results = []
+        expected = {'USC004.txt': 1, 'USC002.txt': 1, 'USC003.txt': 1, 'USC001.txt': 1, 'USC005.txt': 1}
+        for file in os.listdir(TEST_DATA):
+            data = []
+            if file != '.DS_Store':
+                data.extend(self.c.process_file(TEST_DATA, file))
+                results.append(self.c.get_yearly_averages(data))
+        self.fail(self.c.get_year_histogram(results))
+        self.assertEqual(expected, self.c.get_year_histogram(results))
--- source/weather.py
+++ source/weather.py
@@ -1,11 +1,14 @@
 from collections import namedtuple, defaultdict, Counter
+import math
 import numpy
 import os
 
-DESKTOP = os.path.join(os.path.expanduser("~"), "Desktop/")
-ANSWER = DESKTOP + 'answers' + '/'
-
+CODE_EXAM = os.path.abspath(os.path.join(__file__, "../.."))
+SOURCE = os.path.dirname(os.path.realpath(__file__))
+ANSWER = CODE_EXAM + '/' + 'answers' + '/'
+MISSING = -9999
 Day = namedtuple('Day', ['file_name', 'date', 'year', 'high', 'low', 'precip'])
+Avg_Data = namedtuple('Avg_Data', ['name', 'year', 'high', 'low', 'precip'])
 MISSING = -9999
 
 
@@ -24,248 +27,67 @@ class Weather():
             )
         ]
 
-    def process_file(self, file):
+    def process_file(self, path, file):
         result = []
-        with open(os.path.join(DESKTOP + 'wx_data' + '/', file)) as f:
+        with open(os.path.join(path + '/', file)) as f:
             for line in f:
                 result.extend(self.convert_to_days(file, line))
         return result
 
-    def get_weather_data(self):
-        result = []
-        for file in os.listdir(DESKTOP + 'wx_data' + '/'):
-            with open(os.path.join(DESKTOP + 'wx_data' + '/', file)) as f:
-                for line in f:
-                    result.extend(self.convert_to_days(file, line))
-        return result
+    def get_missing_prcp_data(self, file_list):
+        results = Counter(
+            [x.file_name for x in file_list if x.high > MISSING and x.low > MISSING and x.precip == MISSING]
+        )
+        self.write_missingprcpdata(results)
+        return results
 
-    def get_missing_precip_dates(self, file_list):
-        return Counter([x.file_name for x in file_list if x.high > MISSING and x.low > MISSING and x.precip == MISSING])
-
-    def write_precip_dates(self, precips):
+    def write_missingprcpdata(self, results):
         with open(os.path.join(ANSWER, 'MissingPrcpData.out'), 'a') as f:
-            for key, item in precips.items():
+            for key, item in results.items():
                 str_data = '{}\t{}'.format(key, item)
                 f.write(str_data + '\n')
-        return precips
+        return results
 
-    Avg_Data = namedtuple('Avg_Data', ['name', 'year', 'high', 'low', 'precip'])
-
-    def get_averages_data(self, master_list):
-        result = defaultdict(lambda: float(-9999))
+    def get_yearly_averages(self, master_list):
+        results = defaultdict(lambda: ('x', 1, -9999, -9999, -9999))
         for i in range(1985, 2015):
-            result[i] = (
-                self.Avg_Data(
-                    name=master_list[0][0], year=i,  high=10 * numpy.mean([x.high for x in master_list if x.year == i and x.high > MISSING]),
-                    low=10 * numpy.mean([x.low for x in master_list if x.year == i and x.low > MISSING]),
-                    precip=10 * sum([x.precip for x in master_list if x.year == i and x.precip > MISSING]),
+            hi = 0.1 * numpy.mean([x.high for x in master_list if x.year == i and x.high > MISSING])
+            lo = 0.1 * numpy.mean([x.low for x in master_list if x.year == i and x.low > MISSING])
+            prc = 0.1 * sum([x.precip for x in master_list if x.year == i and x.precip > MISSING])
+            results[i] = (
+                    master_list[0][0],
+                    i,
+                    hi if not math.isnan(hi) else -9999,
+                    lo if not math.isnan(lo) else -9999,
+                    prc if prc != 0 else -9999,
                 )
-                )
-        return result, [x for x in result.values()]
+        self.write_yearlyaverages(results)
+        return [Avg_Data(name=x[0], year=x[1], high=x[2], low=x[3], precip=x[4]) for x in results.values()]
 
-    def write_averages_dates(self, result):
-        with open(os.path.join(ANSWER, 'YearlyAverages.out'), 'w') as f:
-            for key, item in result.items():
-                str_data = '{}\t{}\t{:0.2f}\t{:0.2f}\t{:0.2f}'.format(item.name, key, item.high, item.low, item.precip)
+    def write_yearlyaverages(self, results):
+        with open(os.path.join(ANSWER, 'YearlyAverages.out'), 'a') as f:
+            for year, item in results.items():
+                str_data = '{}\t{}\t{:0.2f}\t{:0.2f}\t{:0.2f}'.format(item[0], year, item[2], item[3], item[4])
                 f.write(str_data + '\n')
-        return result
+        return results
 
-    def get_year_histogram(self, file_list):
-        result = []
+    def get_year_histogram(self, yearly_averages):
+        results = []
+        tops = []
+        new_list = [item for sublist in yearly_averages for item in sublist]
         for i in range(1985, 2015):
-            result.append([(x.name, x.year) for x in file_list if x == max([y.high for y in file_list if y.year == i])])
-            # result.append((file_list[i].name, i, max([x.high for x in file_list.values() if x.year == i])))
-        return result
+            for file in os.listdir(path):
+                if file != 'DS_Store':
+
+            avg_high = [x.high for x in new_list if x.year == i and x.high > MISSING]
+            if avg_high:
+                maximum_avg_high = max(avg_high)
+            else:
+                maximum_avg_high = -9999
+            results.append([(x.name, x.year) for x in new_list if x.year == i and x.high == maximum_avg_high and x.high > -9999])
 
 
-    #         high = [(x.file_name, x.high) for x in master_list if x.date == i and x.high > -MISSING]
-    #         low = [(x.file_name, x.low) for x in master_list if x.date == i and x.low > MISSING]
-    #         precip = [(x.file_name, x.precip) for x in master_list if x.precip > MISSING]
-    #         names_high = set([x[0] for x in high])
-    #         names_low = set([x[0] for x in low])
-    #         names_precip = set([x[0] for x in precip])
-    #         result_high = defaultdict(lambda: float(MISSING))
-    #         result_low = defaultdict(lambda: float(MISSING))
-    #         result_precip = defaultdict(lambda: float(MISSING))
-    #         result = {}
-    #         for j in names_high:
-    #             result_high[j] = numpy.mean([x[1] for x in high if x[0] == j])
-    #         for j in names_low:
-    #             result_low[j] = numpy.mean([x[1] for x in low if x[0] == j])
-    #         for j in names_high:
-    #             result_precip[j] = numpy.mean([x[1] for x in precip if x[0] == j])
-    #         all_names = set(list(names_high) + list(names_low) + list(names_precip))
-    #         for name in all_names:
-    #             result[name] = [result_high[name], result_low[name], result_precip[name]]
-    #         return result
-
-
-
-#     def count_missing_precipitation(self, list_of_days):
-#         count = 0
-#         for day in list_of_days:
-#             if int(day.high) > MISSING and int(day.low) > MISSING and int(day.precip) == MISSING:
-#                 count += 1
-#         return count
-
-#     def write_missing_precip(self, list_of_days, count):
-#         answer = DESKTOP + 'answers/'
-#         result = []
-#         with open(os.path.join(answer, 'MissingPrcpData.out'), 'a') as f:
-#             f.write(list_of_days[0].file_name + "\t" + str(count) + '\n')
-#             result.append(str(count))
-#         return result
-
-#     def get_max_min_by_year(self, list_of_days):
-#         years_high = defaultdict(list)
-#         years_low = defaultdict(list)
-#         for day in list_of_days:
-#             if day.high > MISSING:
-#                 years_high[day.date[:4]].append(float(day.high))
-#             if day.low > MISSING:
-#                 years_low[day.date[:4]].append(float(day.low))
-#         return self.get_averages(years_high, years_low)
-
-#     def get_averages(self, years_high, years_low):
-#         high_avg = defaultdict(lambda: float(-9999))
-#         low_avg = defaultdict(lambda: float(-9999))
-#         for i in range(1985, 2015):
-#             key = str(i)
-#             if len(years_high[key]) > 0:
-#                 high_avg[key] = sum(years_high[key])/len(years_high[key])
-#             if len(years_low[key]) > 0:
-#                 low_avg[key] = sum(years_low[key])/len(years_low[key])
-#         return high_avg, low_avg
-
-#     def get_total_precip(self, list_of_days):
-#         total_precip = defaultdict(list)
-#         final_precip = defaultdict(lambda: int(-9999))
-#         for day in list_of_days:
-#             total_precip[day.date[:4]].append(float(day.precip))
-#         for i in range(1985, 2015):
-#             key = str(i)
-#             if total_precip[key]:
-#                 final_precip[key] = sum(total_precip[key])/len(total_precip[key])
-#         return final_precip
-
-#     def write_answer_2(self, file_name, high_avg, low_avg, total_precip):
-#         result = []
-#         with open(os.path.join(DESKTOP + 'answers/', 'YearlyAverages.out'), 'a') as f:
-#             for i in range(1985, 2015):
-#                 key = str(i)
-#                 string_data = '{}\t{}\t{:0.2f}\t{:0.2f}\t{:0.2f}'.format(
-#                     file_name, key, high_avg[key], low_avg[key], total_precip[key])
-#                 f.write(string_data + '\n')
-#                 result.append(string_data.split('\t'))
-#         return result
-
-#     def count_all(self, years_list):
-#         years_high = defaultdict(lambda: float(MISSING))
-#         years_low = defaultdict(lambda: float(9999))
-#         years_precip = defaultdict(lambda: float(MISSING))
-#         count_high = defaultdict(int)
-#         count_low = defaultdict(int)
-#         count_precip = defaultdict(int)
-#         for value in years_list:
-#             if float(value[2]) > MISSING:
-#                 years_high[value[1]] = max(float(value[2]), years_high[value[1]])
-#             if float(value[3]) > MISSING:
-#                 years_low[value[1]] = min(float(value[3]), years_low[value[1]])
-#             if float(value[4]) > MISSING:
-#                 years_precip[value[1]] = max(float(value[4]), years_precip[value[1]])
-#         for value in years_list:
-#             if years_high[value[1]] == float(value[2]) and float(value[2]) > MISSING:
-#                 count_high[value[1]] += 1
-#             if years_low[value[1]] == float(value[3]) and float(value[3]) > MISSING:
-#                 count_low[value[1]] += 1
-#             if years_precip[value[1]] == float(value[4]) and float(value[4]) > MISSING:
-#                 count_precip[value[1]] += 1
-#         return count_high, count_low, count_precip
-
-#     def write_answer_3(self, years_high, years_low, years_precip):
-#         result = []
-#         with open(os.path.join(DESKTOP + 'answers', 'YearHistogram.out'), 'a') as f:
-#             for i in range(1985, 2015):
-#                 key = str(i)
-#                 string_data = '{}\t{}\t{}\t{}'.format(
-#                     key, years_high[key], years_low[key], years_precip[key]
-#                         )
-#                 f.write(string_data + '\n')
-#                 result.append(string_data.split('\t'))
-#         return result
-
-#     def draw_histogram(self, counts):
-#         with open(os.path.join(DESKTOP + 'answers/',  'YearHistogram_draw.txt'), 'a') as f:
-#             for i in range(1985, 2015):
-#                 key = str(i)
-#                 bar = '{}'.format(key) + counts[key] * '#' + '\n'
-#                 f.write(bar)
-
-#     def calculate_pearson(self, weather, yields):
-#         import math
-#         years = len(weather)
-#         weather_station_avg = (sum(weather)/len(weather))
-#         yield_avg = (sum(yields)/len(yields))
-#         diffprod = 0
-#         weather_diff2 = 0
-#         yield_diff2 = 0
-#         for year in range(years):
-#             weather_diff = weather[year] - weather_station_avg
-#             yield_diff = yields[year] - yield_avg
-#             diffprod += weather_diff * yield_diff
-#             weather_diff2 += weather_diff * weather_diff
-#             yield_diff2 += yield_diff * yield_diff
-#         return diffprod / math.sqrt(weather_diff2 * yield_diff2)
-
-#     def pearson(self, years_list, check):
-#         data = {}
-#         weather = defaultdict(float)
-#         with open(os.path.join(DESKTOP + 'yld_data/' + 'US_corn_grain_yield.txt')) as f:
-#             for line in f:
-#                 new_list = line.strip().split('\t')
-#                 data[new_list[0]] = float(new_list[1])
-#         for file in years_list:
-#             weather[file[1]] = float(file[check])
-#         weather_list = [weather.get(x, 0) for x in weather.keys()]
-#         yield_list = [data.get(x, 0) for x in data.keys()]
-#         return self.calculate_pearson(weather_list, yield_list)
-
-#     def write_answer_4(self, years_list):
-#         result = []
-#         with open(os.path.join(DESKTOP + 'answers/',  'Correlations.out'), 'a') as f:
-#             for year in years_list:
-#                 string_data = '{}\t{}\t{:0.2f}\t{:0.2f}\t'.format(
-#                     year[0], self.pearson(years_list, 2), self.pearson(years_list, 3), self.pearson(years_list, 4)
-#                 )
-#                 result.append(string_data.strip('\t'))
-#             for value in sorted(set(result)):
-#                 f.write(value + '\n')
-
-# if __name__ == "__main__":
-#     path_wx_data = DESKTOP + 'wx_data/'
-#     path_yld_data = DESKTOP + 'yld_data/'
-#     try:
-#         os.remove(DESKTOP + 'answers/' + 'YearlyAverages.out')
-#         os.remove(DESKTOP + 'answers/' + 'MissingPrcpData.out')
-#         os.remove(DESKTOP + 'answers/' + 'YearHistogram.out')
-#         os.remove(DESKTOP + 'answers/' + 'YearHistogram_draw.txt')
-#         os.remove(DESKTOP + 'answers/' + 'Correlations.out')
-#     except:
-#         pass
-#     g = Weather()
-#     for filename in os.listdir(path_wx_data):
-#         data = []
-#         new_data = []
-#         years_list = []
-#         with open(os.path.join(path_wx_data, filename)) as inputfile:
-#             for line in inputfile:
-#                 data.append(line.strip().split('\t'))
-#             list_of_days = g.convert_to_days(filename, data)
-#             count = g.count_missing_precipitation(list_of_days)
-#             g.write_missing_precip(list_of_days, count)
-#             high, low = g.get_max_min_by_year(list_of_days)
-#             precip = g.get_total_precip(list_of_days)
-#             years_list.extend(g.write_answer_2(filename, high, low, precip))
-#             years_high, years_low, years_precip = g.count_all(years_list)
-#             g.write_answer_4(years_list)
-#     g.write_answer_3(years_high, years_low, years_precip)
+    def write_yearhistogram(self, year_histogram):
+        with open(os.path.join(ANSWER, 'YearHistogram.out'), 'a') as f:
+            for key, item in year_histogram:
+                str_data = '{}\t{}\t{}'.format(item[0], item[1], item[3])
